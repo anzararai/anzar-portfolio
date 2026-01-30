ba2d53798d123af07a132003c7289b447febbecc
anzararai <anzarai9503@gmail.com>
2026-01-31 00:49:34 +0530

    Fixed ESLint config

--- frontend/components/Contact.jsx
+++ frontend/components/Contact.jsx
@@ -20,7 +20,7 @@ const Contact = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/contact` || 'http://localhost:5000/api/contact' , {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
