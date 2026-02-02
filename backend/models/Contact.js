4b2196fc33fb40d784c33894a285a2888b89ace7
anzararai <anzarai9503@gmail.com>
2026-02-03 02:00:39 +0530

    some minor update in schema

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -6,7 +6,7 @@ const contactSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Name is required'],
       trim: true,
-      minlength: [2, 'Name must be at least 2 characters'],
+      minlength: [1, 'Name must be at least 2 characters'],
       maxlength: [50, 'Name cannot exceed 50 characters']
     },
 
@@ -22,7 +22,7 @@ const contactSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Message is required'],
       trim : true,
-      minlength : [5, 'Message must be at least 5 characters'],
+      minlength : [1, 'Message must be at least 5 characters'],
       maxlength : [1000, 'Message cannot exceed 1000 characters']
     },
 
