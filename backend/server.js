e13366448066eab8f1674c9c014cb22c8dae0d97
anzararai <anzarai9503@gmail.com>
2026-02-03 01:07:48 +0530

    fixed some error

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-// require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
 
 const connectDB = require('./config/db');
 const contactRoutes = require('./routes/contactRoutes');
